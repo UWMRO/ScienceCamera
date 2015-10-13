a49d97be8de6c9c74fef634dde829c6efec25cf9
Joseph Huehnerhoff <jwhueh@gmail.com>
2015-10-12 18:00:25 -0700

    add camera server

---
+++ include/camera_reactor.py
@@ -0,0 +1,37 @@
+"""The most basic chat protocol possible.
+
+run me with twistd -n -y camera_reactor.py, and then connect with multiple
+telnet clients to port 26999
+"""
+
+from twisted.protocols import basic
+
+
+
+class CameraReciever(basic.LineReceiver):
+    def connectionMade(self):
+        print "Got new client!"
+        self.factory.clients.append(self)
+
+    def connectionLost(self, reason):
+        print "Lost a client!"
+        self.factory.clients.remove(self)
+
+    def lineReceived(self, line):
+        print "received", repr(line)
+        for c in self.factory.clients:
+            c.message(line)
+
+    def message(self, message):
+        self.transport.write(message + '\n')
+
+
+from twisted.internet import protocol
+from twisted.application import service, internet
+
+factory = protocol.ServerFactory()
+factory.protocol = CameraReciever
+factory.clients = []
+
+application = service.Application("Evora")
+internet.TCPServer(26999, factory).setServiceParent(application)
---
+++ include/evora.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python
+
+import andor
+
+class evora(object):
+
+    def connect():
+        print andor.GetAvailableCameras()
+        camHandle = andor.GetCameraHandle(0)
+        print camHandle
+        print 'set camera:', andor.SetCurrentCamera(camHandle[1])
+        print 'Init:', andor.Initialize("/usr/local/etc/andor")
+        
+        #time.sleep(2)
+        
+        print 'Status:', andor.GetStatus()
+        
+        print 'SetAcquisitionMode:', andor.SetAcquisitionMode(1);
+        
+        print 'SetShutter:', andor.SetShutter(1,0,50,50);
+
+    def get_cooler():
+        # index on [result[0] - andor.DRV_TEMPERATURE_OFF]
+        coolerStatusNames = ('Off', 'NotStabilized', 'Stabilized',
+                         'NotReached', 'OutOfRange', 'NotSupported',
+                         'WasStableNowDrifting')
+
+        result = andor.GetTemperatureF()
+        print coolerStatusNames[result[0] - andor.DRV_TEMPERATURE_OFF], result[1]
+        return result[0]
+        print '2'
+        result = andor.GetTemperatureStatus()
+        print result
+
+
+    def cooler(setPoint=None):
+
+        result = get_cooler()
+
+        if setPoint is not None:
+            if result == andor.DRV_TEMPERATURE_OFF:
+                andor.CoolerON()
+            andor.SetTemperature(setPoint)
+            get_cooler()
+
+    def warmup():
+        pass
+
+    def shutdown():
+        pass
+
+
+
+if __name__ == "__main__":
+    e = evora()
+    e.startup()
+
+    e.
