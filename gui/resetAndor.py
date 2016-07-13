b9ab453a9b86024a94cd37fb2dcb5f07f6132d0b
tristan3214 <tristan3214@live.com>
2016-07-13 13:48:32 -0700

    usbreset

--- gui/resetAndor.py
+++ gui/resetAndor.py
@@ -7,30 +7,28 @@ class resetAndor(object):
 
 	def __init__(self):
 		self.cwd = os.getcwd()
-		self.bus = 0
-        	self.device = 0
-        	self.cwd = ''
-        	self.foundStr = ''
+		self.bus = 0 #bus number
+        	self.device = 0 #device number
+        	self.foundStr = '' #parsed line with Andor ino
 	
+	#run lsusb in terminal, parse output and pull out bus and device numberes
+	#for Andor, save globally
 	def getInfo(self):
 		devices = check_output('lsusb')
-		print devices
 		devices = devices.split('\n')
-		print devices
 		for usb in devices:
 			if re.search('Andor',usb):
 				self.foundStr = usb
 		self.foundStr = self.foundStr.split(' ')
 		self.bus = self.foundStr[1]
 		self.device = self.foundStr[3]
-
+	
+	#use found Andor bus and device numbers to run usbreset and reset Andor
 	def restart(self):
 		resetString = ' /dev/bus/usb/' + str(self.bus) + '/' + str(self.device).strip(':')
 		command = os.getcwd() + '/usbreset' + resetString
-		print command
-		print os.path.join(os.getcwd(),'usbreset') + resetString
-		#subprocess.Popen(['sudo',os.path.join(os.getcwd(),'usbreset'),resetString])
 		os.system(command)
+
 if __name__ == "__main__":
 	g = resetAndor()
 	g.getInfo()
