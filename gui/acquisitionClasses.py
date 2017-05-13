79140b074bcb1f77621e7caa6a965237afa99a83
Tristan Hillis <tristan3214@live.com>
2017-05-13 10:00:46 -0700

    tjh

--- gui/acquisitionClasses.py
+++ gui/acquisitionClasses.py
@@ -61,7 +61,7 @@ class ImageQueueWatcher(threading.Thread, object):
             if self.exposeClass.imageQueue.qsize() == 0:
                 print("ImageQueueWatcher WAITING")
                 self.exposeClass.imageAddedEvent.wait()
-            while self.exposeClass.imageQueue.qsize() > 0:
+            while self.exposeClass.imageQueue.qsize() > 0 and self.exposeClass.abort:
                 line = str(self.exposeClass.imageQueue.get()).split(";")
                 image_path = line[0]
                 image_name = line[1]
