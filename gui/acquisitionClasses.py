79cf8816d356b0b49d6cbc34aa8cade810fb7b99
Tristan Hillis <tristan3214@live.com>
2017-05-27 14:47:56 -0700

    tjh

--- gui/acquisitionClasses.py
+++ gui/acquisitionClasses.py
@@ -89,7 +89,7 @@ class ImageQueueWatcher(threading.Thread, object):
         print("FAILED TO RETRIEVE")
         pass # do nothing
 
-class PlotterWatcher(Thread, object):
+class PlotterWatcher(threading.Thread, object):
     def __init__(self, exposeClass):
         threading.Thread.__init__(self)
         self.exposeClass = exposeClass
