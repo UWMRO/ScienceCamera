ed95139912259c29a8aeeb6e42707087ba84233d
Tristan Hillis <tristan3214@live.com>
2017-06-02 17:44:14 -0700

    tjh

--- gui/acquisitionClasses.py
+++ gui/acquisitionClasses.py
@@ -73,10 +73,14 @@ class ImageQueueWatcher(threading.Thread, object):
                 print("Transfering:", image_name)
 
                 #savedImage, d = self.exposeClass.transferImage(image_path, image_name, image_type)
-                self.exposeClass.ftpLayer.sendCommand("get %s %s %s %s" % (image_name, self.exposeClass.saveDir,
-                                                                           self.exposeClass.currentImage+".fits",
-                                                                           image_type)).addCallback(self.transferCallback)
-                
+                if image_type != 'real':
+                    self.exposeClass.ftpLayer.sendCommand("get %s %s %s %s" % (image_name, self.exposeClass.saveDir,
+                                                                               self.exposeClass.currentImage+".fits",
+                                                                               image_type)).addCallback(self.transferCallback)
+                else:
+                    self.exposeClass.ftpLayer.sendCommand("get %s %s %s %s" % (image_name, self.exposeClass.saveDir,image_name,
+                                                                               image_type)).addCallback(self.transferCallback)
+                    
                 #print("Plotting:", savedImage, "shortly.")
                 #print("Size of queue", self.exposeClass.imageQueue.qsize())
                 #self.exposeClass.transferDone.wait()
