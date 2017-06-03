9e9c77c304e635f9d62df3cbed5e67ffacd8fa35
Tristan Hillis <tristan3214@live.com>
2017-06-02 19:41:01 -0700

    tjh

---
+++ gui/evora_ftp_server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+from __future__ import print_function, division, absolute_import
+
+from twisted.protocols import basic
+from twisted.internet import reactor
+
+# ftp server imports
+from twisted.protocols.ftp import FTPFactory
+from twisted.protocols.ftp import FTPRealm
+from twisted.cred.portal import Portal
+from twisted.cred.checkers import AllowAnonymousAccess
+
+import AddLinearSpacer as als
+
+p = Portal(FTPRealm("/home/mro/storage/evora_data/"), [AllowAnonymousAccess()])
+f = FTPFactory(p)
+f.timeOut = None
+reactor.listenTCP(als.FTP_TRANSFER_PORT, f)
+
+reactor.run()
