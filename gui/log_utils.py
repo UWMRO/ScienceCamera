664c25b257c1627c18e071772b4c7c1b91109817
Spencer Burris <sburris@posteo.net>
2020-02-27 13:31:01 -0800

    Remove useless parenthesis

--- gui/log_utils.py
+++ gui/log_utils.py
@@ -13,124 +13,123 @@ def get_log_str(command, prePost):
           log tab.  If string is not made then it returns None.
     """
     command = command.split(" ")
-    if(prePost == 'pre'):  # command is split with by white space
+    if prePost == 'pre':  # command is split with by white space
         key = command[0]
 
-        if(key == 'expose'):
-            itime = float(command[3])
+        if key == 'expose':
             return "Exposing for time %.2f sec" % itime
-        if(key == 'real'):
+        if key == 'real':
             itime = float(command[3])
             return "Starting real time expsoures with %.2f sec" % itime
-        if(key == 'series'):
+        if key == 'series':
             itime = float(command[3])
             number = int(command[2])
             return "Exposing for %d images with time %.2f sec" % (number, itime)
-        if(key == 'setTEC'):
+        if key == 'setTEC':
             temp = float(command[1])
             return "Setting temperature to %.1f C" % temp
-        if(key == 'warmup'):
+        if key == 'warmup':
             return "Turning off cooler"
-        if(key == 'abort'):
+        if key == 'abort':
             return "Aborting current exposure..."
-        if(key == 'filter'):
+        if key == 'filter':
             key2 = command[1]
-            if(key2 == 'home'):
+            if key2 == 'home':
                 return "Starting homing sequence..."
-            if(key2 == 'move'):
+            if key2 == 'move':
                 filter = command[2]
                 return "Moving to filter %s" % filter
-            if(key2 == 'getFilter'):
+            if key2 == 'getFilter':
                 return "Getting filter position..."
-            if(key2 == 'connect'):
+            if key2 == 'connect':
                 return "Please home filter..."
 
-    if(prePost == 'post'):  # command has a key then is followed by relavent information delimited with commas
+    if prePost == 'post':  # command has a key then is followed by relavent information delimited with commas
         key = command[0]
         logger.debug("key from post: " + str(key))
         stats = command[1].split(",")
         logger.debug("Stats in log:" + str(stats))
-        if(key == 'status'):
-            if(int(stats[0]) == 20002):  # 20002 is "success" to Evora
+        if key == 'status':
+            if int(stats[0]) == 20002:  # 20002 is "success" to Evora
                 return "Camera already initialized connecting..."
-            elif(int(stats[0]) == 20075):
+            elif int(stats[0]) == 20075:
                 return "Camera uninitialized this will take a few..."
             else:
                 return "Camera drivers reporting incorrectly please run reinstall..."
-        if(key == 'expose'):
+        if key == 'expose':
             # at the end of stats is the image name
             name = stats[-1]
             itime = float(stats[2])
             results = int(stats[0])
-            if(results == 1):  # 1 for successful exposure
+            if results == 1:  # 1 for successful exposure
                 return "\"%s\" completed with time %.2f sec" % (name, itime)
             else:
                 return "\"%s\" failed to expose..." % name
-        if(key == 'real'):
+        if key == 'real':
             results = stats[0]
             return "Real time exposure successfully done..."
-        if(key == 'series'):
+        if key == 'series':
             results = stats[0]
             return "Done take series images..."
-        if(key == 'seriesSent'):
+        if key == 'seriesSent':
             name = stats[-1]
             itime = float(stats[1])
             return "\"%s\" completed with time %.2f sec" % (name, itime)
-        if(key == 'connect'):
-            if(int(stats[0]) == 20002):  # 20002 is "success" Evora
+        if key == 'connect':
+            if int(stats[0]) == 20002:  # 20002 is "success" Evora
                 return "Initialization completed..."
             else:
                 return "Initialization failed..."
-        if(key == 'connectLost'):
+        if key == 'connectLost':
             return "Disconnected from camera normally..."
-        if(key == 'connectFailed'):
+        if key == 'connectFailed':
             return "Disconnected from camera suddenly..."
-        if(key == 'getTEC'):
+        if key == 'getTEC':
             pass
-        if(key == 'setTEC'):
+        if key == 'setTEC':
             temp = float(stats[0])
             return "Successfully set cooler to %.1f C" % temp
-        if(key == 'warmup'):
+        if key == 'warmup':
             return "Successfully warming up..."
-        if(key == 'temp'):
+        if key == 'temp':
             results = stats[0]  # 1 for success 0 for failure
-            if(results == 1):
+            if results == 1:
                 return "Successfully shutdown cooler..."
             else:
                 return "Failure in setting cooler down..."
-        if(key == 'startup'):
+        if key == 'startup':
             if int(stats[0]) == 20002:
                 return "Camera initialization successful..."
             else:
                 return "Camera initialization went wrong check the server..."
-        if(key == 'shutdown'):
+        if key == 'shutdown':
             results = stats[0]
             return "Successfully shutdown camera..."
-        if(key == 'abort'):
+        if key == 'abort':
             results = stats[0]
             return "Successfully aborted exposure..."
-        if(key == 'filter'):
+        if key == 'filter':
             key2 = command[1]
             stats = command[2].split(",")
-            if(key2 == 'home'):
-                if(int(stats[0]) == 1):
+            if key2 == 'home':
+                if int(stats[0]) == 1:
                     return "Successfully homed..."
                 else:
                     return "Failed to home, try again..."
-            if(key2 == 'move'):
-                if(bool(stats[0])):
+            if key2 == 'move':
+                if bool(stats[0]):
                     return "Successfully moved filter..."
                 else:
                     return "Failed to move filter..."
-            if(key2 == 'findPos'):
+            if key2 == 'findPos':
                 return "Adjusting fitler this can take awhile..."
-            if(key2 == 'getFilter'):
+            if key2 == 'getFilter':
                 key3 = command[2]
                 stats = command[3].split(",")
-                if(key3 == 'report'):
+                if key3 == 'report':
                     filter = stats[0]
                     return "At filter %s" % filter
-                if(key3 == 'finding'):
+                if key3 == 'finding':
                     filter = stats[0]
                     pos = int(stats[1])
                     return "Filter settleing in on position %d, or filter %s..." % (pos, filter)
@@ -138,9 +137,9 @@ def get_log_str(command, prePost):
                     filter = stats[0]
                     pos = int(stats[1])
                     return "At position %d, setting filter to %s..." % (pos, filter)
-            if(key2 == 'connectLost'):
+            if key2 == 'connectLost':
                 return "Connection to filter lost normally..."
-            if(key2 == 'connectFailed'):
+            if key2 == 'connectFailed':
                 return "Connection to filter failed suddenly..."
     return None
 
@@ -155,18 +154,21 @@ def time_stamp():
     hour = ""
     minute = ""
     second = ""
-    if(time.hour < 10):
+    if time.hour < 10:
         hour += "0" + str(time.hour)
     else:
         hour += str(time.hour)
-    if(time.minute < 10):
+
+    if time.minute < 10:
         minute += "0" + str(time.minute)
     else:
         minute += str(time.minute)
-    if(time.second < 10):
+
+    if time.second < 10:
         second += "0" + str(time.second)
     else:
         second += str(time.second)
+
     stamp = "[%s:%s:%s]" % (hour, minute, second)
     return stamp
 
