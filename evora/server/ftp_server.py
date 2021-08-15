61a59e520d7138bf06e26df94e72b1422b911bea
Spencer Burris <sburris@posteo.net>
2021-08-15 11:37:11 -0700

    Error on missing evora_data directory

--- evora/server/ftp_server.py
+++ evora/server/ftp_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 from __future__ import absolute_import, division, print_function
 
+from os.path import isdir
 from twisted.cred.checkers import AllowAnonymousAccess
 from twisted.cred.portal import Portal
 from twisted.internet import reactor
@@ -9,9 +10,17 @@ from twisted.protocols.ftp import FTPFactory, FTPRealm
 
 from evora.common import netconsts
 
-p = Portal(FTPRealm("/home/mro/storage/evora_data/"), [AllowAnonymousAccess()])
-f = FTPFactory(p)
-f.timeOut = None
-reactor.listenTCP(netconsts.FTP_TRANSFER_PORT, f)
+# TODO: this needs to be specified some other way
+# Does not exist on non-observatory computers
+data_path = "/home/mro/storage/evora_data/"
+
+if isdir(data_path):
+    p = Portal(FTPRealm(data_path), [AllowAnonymousAccess()])
+    f = FTPFactory(p)
+    f.timeOut = None
+    reactor.listenTCP(netconsts.FTP_TRANSFER_PORT, f)
+else:
+    print("Directory at '" + data_path + "' does not exist, exiting...")
+    quit()
 
 reactor.run()
